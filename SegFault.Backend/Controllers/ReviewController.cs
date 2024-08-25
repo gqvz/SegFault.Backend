342cfe4c3a0dfdad19a304ba57a68a66516c73a5
Garvit Sharma <70444445+toasty1307@users.noreply.github.com>
2024-08-25 06:00:52 +0530

    reviews done?

--- SegFault.Backend/Controllers/ReviewController.cs
+++ SegFault.Backend/Controllers/ReviewController.cs
@@ -30,6 +30,32 @@ public class ReviewController(SessionService sessionService, ReviewService revie
         var result = await reviewService.Reviews.FindAsync(r => r.ReviewerId == enrollmentNumber);
         return await result.ToListAsync();
     }
-    
-    
+
+    [HttpDelete("{reviewId}")]
+    public async Task<IActionResult> DeleteReview([FromRoute] string reviewId, [FromHeader(Name = "Auth")] string auth)
+    {
+        var enrollmentNumber = Convert.ToUInt32(auth.Split()[0]);
+
+        var reviews = await reviewService.Reviews.FindAsync(r => r.Id == reviewId);
+        var review = reviews.First();
+        if (review.ReviewerId != enrollmentNumber)
+            return Unauthorized();
+        await reviewService.Reviews.DeleteOneAsync(r => r.Id == reviewId);
+        return Ok();
+    }
+
+    [HttpPost("items/{itemId}")]
+    public async Task<IActionResult> PostItemReview([FromBody] Review review)
+    {
+        review.Id = Guid.NewGuid().ToString();
+        await reviewService.Reviews.InsertOneAsync(review); // ik i should verify the user but wtv
+        return Ok();
+    }
+
+    [HttpGet("items/{itemId}")]
+    public async Task<List<Review>> GetItemReviews([FromRoute] string itemId)
+    {
+        var reviews = await reviewService.Reviews.FindAsync(r => r.Target == $"items/{itemId}");
+        return await reviews.ToListAsync();
+    }
 }
